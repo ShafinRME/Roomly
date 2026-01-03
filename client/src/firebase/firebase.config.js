f221542543d5fd70845e63fa7f9857e432b596b5
ShafinRME <shafin.rmedu@gmail.com>
2026-01-03 12:15:16 +0600

    Alhamdulillah.Stay Vista initial setup is done

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,5 +1,10 @@
-import { initializeApp } from 'firebase/app'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
@@ -9,4 +14,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 }
 
-export const app = initializeApp(firebaseConfig)
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
