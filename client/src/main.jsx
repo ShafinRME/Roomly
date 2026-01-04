2244d419f6d43ecfb0459e1a85eac78a30ef7ab3
ShafinRME <shafin.rmedu@gmail.com>
2026-01-04 22:43:15 +0600

    navbar dynamic indicator is done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
@@ -8,11 +9,17 @@ import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
 import { HelmetProvider } from 'react-helmet-async'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
   </HelmetProvider>
 )
