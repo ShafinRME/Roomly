379e83bf74c2611231f0d08699982b62cb2b2da4
ShafinRME <shafin.rmedu@gmail.com>
2026-01-10 10:35:44 +0600

    Saved users in DB

--- server/index.js
+++ server/index.js
@@ -80,6 +80,37 @@ async function run() {
       }
     })
 
+    // save a user data in db
+    app.put('/user', async (req, res) => {
+      const user = req.body
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query)
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // if existing user try to change his role
+          const result = await usersCollection.updateOne(query, {
+            $set: { status: user?.status },
+          })
+          return res.send(result)
+        } else {
+          // if existing user login again
+          return res.send(isExist)
+        }
+      }
+
+      // save user for the first time
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      res.send(result)
+    })
+
 
     // Get all rooms
     app.get('/rooms', async (req, res) => {
