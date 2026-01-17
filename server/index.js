3a854722e3d33c08f30f9a4afdeceac5ee0d5518
ShafinRME <shafin.rmedu@gmail.com>
2026-01-17 15:56:36 +0600

    verifyAdmin middle were created

--- server/index.js
+++ server/index.js
@@ -50,6 +50,19 @@ async function run() {
     const roomsCollection = client.db('stayvista').collection('rooms')
     const usersCollection = client.db('stayvista').collection('users')
 
+    // verify admin middleware
+    const verifyAdmin = async (req, res, next) => {
+      console.log('hello')
+      const user = req.user
+      const query = { email: user?.email }
+      const result = await usersCollection.findOne(query)
+      console.log(result?.role)
+      if (!result || result?.role !== 'admin')
+        return res.status(401).send({ message: 'unauthorized access!!' })
+
+      next()
+    }
+
     // auth related api
     app.post('/jwt', async (req, res) => {
       const user = req.body
