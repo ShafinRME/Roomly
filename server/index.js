aa9cb9cb66e220ce7328ecca3144c76d60b99c99
ShafinRME <shafin.rmedu@gmail.com>
2026-01-19 12:34:47 +0600

    delete booking api created

--- server/index.js
+++ server/index.js
@@ -267,6 +267,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete a booking
+    app.delete('/booking/:id', verifyToken, async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
